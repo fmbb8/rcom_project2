e446c2723179e3d9afa5f359861d6654d484322e
denizlg24 <denizlg24@gmail.com>
2025-12-05 15:08:29 +0000

    removed up

--- bashscripts/tux3.sh
+++ bashscripts/tux3.sh
@@ -2,7 +2,7 @@
 
 echo "Configuring tux3..."
 
-sudo ifconfig if_e1 172.16.60.1/24 up
+sudo ifconfig if_e1 172.16.60.1/24
 
 sudo route add -net 172.16.61.0/24 gw 172.16.60.254
 
--- bashscripts/tux4.sh
+++ bashscripts/tux4.sh
@@ -2,8 +2,8 @@
 
 echo "Configuring tux4 (router)..."
 
-sudo ifconfig if_e1 172.16.60.254/24 up
-sudo ifconfig if_e2 172.16.61.253/24 up
+sudo ifconfig if_e1 172.16.60.254/24
+sudo ifconfig if_e2 172.16.61.253/24
 
 sudo sysctl -w net.ipv4.ip_forward=1
 
