9e0858758c7b471997b615380ebe3631762a8bf3
Deniz Lopes Güneş <112947911+denizlg24@users.noreply.github.com>
2025-12-10 08:36:29 +0000

    Update interface names in switch_commands.sh

--- bashscripts/switch_commands.sh
+++ bashscripts/switch_commands.sh
@@ -7,21 +7,21 @@
 
 # 3. Assign Ports to Bridge 60 (Tux3-e1 & Tux4-e1)
 # Remove from default bridge first, then add to bridge60
-/interface bridge port remove [find interface=etherA]
-/interface bridge port add bridge=bridge60 interface=etherA
+/interface bridge port remove [find interface=etherTUX3_E1]
+/interface bridge port add bridge=bridge60 interface=etherTUX3_E1
 
-/interface bridge port remove [find interface=etherB]
-/interface bridge port add bridge=bridge60 interface=etherB
+/interface bridge port remove [find interface=etherTUX4_E1]
+/interface bridge port add bridge=bridge60 interface=etherTUX4_E1
 
 # 4. Assign Ports to Bridge 61 (Tux4-e2, Tux2-e1 & RC)
-/interface bridge port remove [find interface=etherC]
-/interface bridge port add bridge=bridge61 interface=etherC
+/interface bridge port remove [find interface=etherTUX2_E1]
+/interface bridge port add bridge=bridge61 interface=etherTUX2_E1
 
-/interface bridge port remove [find interface=etherD]
-/interface bridge port add bridge=bridge61 interface=etherD
+/interface bridge port remove [find interface=etherTUX4_E2]
+/interface bridge port add bridge=bridge61 interface=etherTUX4_E2
 
-/interface bridge port remove [find interface=etherE]
-/interface bridge port add bridge=bridge61 interface=etherE
+/interface bridge port remove [find interface=etherRC_ETHER2]
+/interface bridge port add bridge=bridge61 interface=etherRC_ETHER2
 
 # 5. Verify
 /interface bridge port print brief
